6e186a01fcaad2388019c9efabb38d9755ad0f83
ShBr2k <shbr2k@gmail.com>
2019-10-06 14:34:59 +0300

    Test UPD02.

--- src/HW/LoopsArray/HW05.java
+++ src/HW/LoopsArray/HW05.java
@@ -9,7 +9,9 @@
  */
 package HW.LoopsArray;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 import static HW.zTools.Readers.stringReader;
@@ -19,103 +21,117 @@ import static HW.zTools.Readers.stringReader;
  * Class HW05 - main for current task
  *
  * @author Roman Shamakhin
- * @version 0.10 04 Oct 2019
+ * @version 0.11 04 Oct 2019
  */
 
 public class HW05 {
+
+    private Car[] arrayCar = new Car[4];    // Create array of instances
 
     /**
      * The Car class provide creation instance, setting and getting access to
      * the fields a class
      */
-    class Car {
+    private class Car {
         private String Type;            // Type of Car
         private Date YearProduction;    // Year production of Car, type is Date
         private float EngineCapacity;   // Engine Capacity
 
-        /** Constructor Car provide creation of default instance */
+        /**
+         * Constructor Car provide creation of instances
+         */
         private Car() {
         }
 
-        /** Method setType provide setting value type for instance */
+        /**
+         * Method setType provide setting value type for instance
+         */
         private void setType(String type) {
             this.Type = type;
         }
 
-        /** Method setYearProduction provide setting value yearProduction for instance */
+        /**
+         * Method setYearProduction provide setting value yearProduction for instance
+         */
         private void setYearProduction(Date yearProduction) {
             this.YearProduction = yearProduction;
         }
 
-        /** Method setEngineCapacity provide setting value EngineCapacity for instance */
+        /**
+         * Method setEngineCapacity provide setting value EngineCapacity for instance
+         */
         private void setEngineCapacity(float engineCapacity) {
             this.EngineCapacity = engineCapacity;
         }
 
-        /** Method getType provide reading value Type of instance */
+        /**
+         * Method getInformation provide reading all values of instance
+         */
+        private String getInformation(int numberCar) {
+            String returnInformation = "Type of car #" + numberCar + ": " + this.getType() +
+                    ". Year of production: " + String.format("%tY", this.getYearProduction()) +
+                    ". Engine capacity: " + this.getEngineCapacity();
+            return returnInformation;
+        }
+
+        /**
+         * Method getType provide reading value Type of instance
+         */
         private String getType() {
             return this.Type;
         }
 
-        /** Method getYearProduction provide reading value YearProduction of instance */
+        /**
+         * Method getYearProduction provide reading value YearProduction of instance
+         */
         private Date getYearProduction() {
             return this.YearProduction;
         }
 
-        /** Method getEngineCapacity provide reading value EngineCapacity of instance */
+        /**
+         * Method getEngineCapacity provide reading value EngineCapacity of instance
+         */
         private float getEngineCapacity() {
             return this.EngineCapacity;
         }
     }
 
-    /** Method runHW05 provide execution of task */
-    public void runHW05() throws Exception {
-
-        Date yearProduction;            // Year production of Car, type is Date
-
-        Car[] arrayCar = new Car[4];    // Create array of instances
-
-        /* Create instance #1 of class Car, fill fields of instance,fill array of instances*/
-        System.out.print("Input Year for Car#1: ");
-        yearProduction = new SimpleDateFormat("yyyy").parse(stringReader());
-        Car car01 = new Car();
-        car01.setType("A");
-        car01.setYearProduction(yearProduction);
-        car01.setEngineCapacity(1.1f);
-        arrayCar[0] = car01;
-
-        /* Create instance #2 of class Car, fill fields of instance,fill array of instances*/
-        System.out.print("Input Year for Car#2: ");
-        yearProduction = new SimpleDateFormat("yyyy").parse(stringReader());
-        Car car02 = new Car();
-        car02.setType("B");
-        car02.setYearProduction(yearProduction);
-        car02.setEngineCapacity(1.2f);
-        arrayCar[1] = car02;
-
-        /* Create instance #3 of class Car, fill fields of instance,fill array of instances*/
-        System.out.print("Input Year for Car#3: ");
-        yearProduction = new SimpleDateFormat("yyyy").parse(stringReader());
-        Car car03 = new Car();
-        car03.setType("C");
-        car03.setYearProduction(yearProduction);
-        car03.setEngineCapacity(1.3f);
-        arrayCar[2] = car03;
-
-        /* Create instance #4 of class Car, fill fields of instance,fill array of instances*/
-        System.out.print("Input Year for Car#4: ");
-        yearProduction = new SimpleDateFormat("yyyy").parse(stringReader());
-        Car car04 = new Car();
-        car04.setType("D");
-        car04.setYearProduction(yearProduction);
-        car04.setEngineCapacity(1.4f);
-        arrayCar[3] = car04;
-
-        /* Show elements of array before sorting*/
-        System.out.println("List of cars before sorting:");
-        for (int i = 0; i < 4; i++) {
-            System.out.println("Type of car #" + i + ": " + arrayCar[i].getType() + ". Year of production: " + String.format("%tY", arrayCar[i].getYearProduction()) + ". Engine capacity: " + arrayCar[i].getEngineCapacity());
+    /**
+     * Method readArrayCar provide showing information of instances from arrayCar
+     */
+    private void readArrayCar(int lengthArrayCar) {
+        for (int i = 0; i < lengthArrayCar; i++) {
+            System.out.println(arrayCar[i].getInformation(i + 1));
         }
+    }
+
+
+    /**
+     * Method runHW05 provide execution of task
+     */
+    public void runHW05() {
+
+        /* Filling array of instances of class Car, value yearProduction input by console,
+           values type, engineCapacity is calculated */
+        for (int i = 0; i < arrayCar.length; i++) {
+            System.out.print("Input Year of Production Car #" + (i + 1) + ": ");
+            // Checking for corrected of value of Year
+            try {
+                Date yearProduction = new SimpleDateFormat("yyyy").parse(stringReader());
+                arrayCar[i] = new Car();
+                arrayCar[i].setType(Character.toString((char) (i + 65)));
+                arrayCar[i].setYearProduction(yearProduction);
+                arrayCar[i].setEngineCapacity(1 + i * 0.1f);
+            } catch (ParseException e) {
+                System.out.println("Value is not Year.");
+                System.exit(0);
+            } finally {
+            }
+        }
+
+        /* Showing elements of array before sorting */
+        System.out.println(System.lineSeparator() + "List of cars before sorting:");
+        readArrayCar(arrayCar.length);
 
         /* Do sorting elements of YearProduction field of array by ascending. Use algorithm 'Sorting by inclusion | Insertion sort'. */
         Car curCar = new Car();                 // Create temporary instance of class Car
@@ -129,16 +145,14 @@ public class HW05 {
                 tmpCar = arrayCar[j];           // Save temporary instance from array [j]
                 arrayCar[j] = arrayCar[j - 1];  // Move instance in array from [j-1] to [j]
                 arrayCar[j - 1] = tmpCar;       // Restore temporary instance to [j-1]
-                j--;
+                j--;                            // use previous index
             }
         }
-        // try to use  Arrays.sort(arrayCar);
 
-        /* Show elements of array after sorting*/
-        System.out.println("List of cars after sorting:");
-        for (int i = 0; i < 4; i++) {
-            System.out.println("Type of car #" + i + ": " + arrayCar[i].getType() + ". Year of production: " + String.format("%tY", arrayCar[i].getYearProduction()) + ". Engine capacity: " + arrayCar[i].getEngineCapacity());
-        }
+        // try to use Arrays.sort(arrayCar);
 
+        /* Showing elements of array after sorting */
+        System.out.println(System.lineSeparator() + "List of cars after sorting:");
+        readArrayCar(arrayCar.length);
     }
 }
